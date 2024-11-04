9829753f79530493571fb62011f50af5e99f233a
Mehedi Hasan <thecodermehedi@gmail.com>
2024-11-04 08:09:01 +0600

    Add MutationConcept.ts with examples of array manipulation and notes on immer library

---
+++ src/MutationConcept.ts
@@ -0,0 +1,26 @@
+//! const arr = ["Mezba bhai", "Mizan bhai"]
+
+//! console.log(arr);
+
+//! arr.push("Rakib bhai");
+
+//! console.log(arr);
+
+//! arr.pop()
+
+//! console.log(arr)
+
+
+//* const arr = ["Mezba vai", "Mizan vai"]
+
+//* console.log(arr)
+
+//* const arr2 = [...arr, "Parvez vai"]
+
+//* console.log(arr2)
+
+//* const arr3 = arr2.filter(i => i !== "Mizan vai");
+
+//* console.log(arr3)
+
+//? There is a library name [immer](https://immerjs.github.io/immer) that handles this stuff better by using draft state.
